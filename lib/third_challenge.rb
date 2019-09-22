731cbc00ab6b58ec080952ead830c974dd7b6698
Jalysa Reason <jalysareason@gmail.com>
2019-09-22 15:42:23 +0000

    Automatically backed up by Learn

--- lib/third_challenge.rb
+++ lib/third_challenge.rb
@@ -40,11 +40,11 @@ def third_challenge
   end
 
   it "fills out the Capulet character role keys with the key/value pairs describing their attributes" do 
-    expect(third_challenge[:capulet][:patriarch].keys).to eq([:name, :age])
-    expect(third_challenge[:capulet][:matriarch].keys).to eq([:name, :age])
-    expect(third_challenge[:capulet][:heroine].keys).to eq([:name, :age, :status])
-    expect(third_challenge[:capulet][:patriarch].values).to eq(["Lord Capulet", "50"])
-    expect(third_challenge[:capulet][:matriarch].values).to eq(["Lady Capulet", "51"])
-    expect(third_challenge[:capulet][:heroine].values).to eq(["Juliet", "15", "alive"])
+    (third_challenge[:capulet][:patriarch].keys)=([:name, :age])
+    (third_challenge[:capulet][:matriarch].keys)=([:name, :age])
+    (third_challenge[:capulet][:heroine].keys)=([:name, :age, :status])
+    (third_challenge[:capulet][:patriarch].values)=(["Lord Capulet", "50"])
+    (third_challenge[:capulet][:matriarch].values)=(["Lady Capulet", "51"])
+    (third_challenge[:capulet][:heroine].values)=(["Juliet", "15", "alive"])
   end
-end
+
